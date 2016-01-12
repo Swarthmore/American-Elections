4fa55493546c52a274cf0c67901cf57977387457
Michael <kappeler.michael@gmail.com>
2016-01-12 15:13:42 -0500

    Updates Bootstrap
    Uses can to deliver bootstrap

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 
-// Register Styles and Scripts
+// Register Style
 function custom_styles() {
 
-  wp_register_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', false, '2.1.1' );
-  wp_enqueue_style( 'bootstrap' );
+  wp_register_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css', false, '3.3.6' );
+  wp_enqueue_style( 'bootstrap-css' );
 
   wp_enqueue_style( 'american-elections-style', get_stylesheet_uri());
 
-  wp_register_script( 'bootstrap-js', get_stylesheet_directory_uri() .'/js/bootstrap.js', array('jquery'), '2.1.1', false );
-  wp_enqueue_script( 'bootstrap-js' );
+  wp_register_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array('jquery'), '3.3.6', false );
+  wp_enqueue_script( 'bootstrap' );
 
   wp_register_script( 'html5', get_stylesheet_directory_uri() .'/js/html5.js', false, '1.6.2', false );
   wp_enqueue_script( 'html5' );
